46b078d2da5e5b293b151a8b599337aee5b11f17
namubirujesca <namubirujesca@gmail.com>
2023-03-08 21:19:26 -0600

    clean up

--- repository.js
+++ repository.js
@@ -1,7 +1,7 @@
 const MongoClient = require('mongodb').MongoClient;
 // const url = 'mongodb://localhost:27017';
-const url = 'mongodb://mongo:27017/dockerize-me';
-// const url = 'mongodb://mongo:27017';
+// const url = 'mongodb://mongo:27017/dockerize-me';
+const url = 'mongodb://mongo:27017';
 let db = null;
 // connect to mongo
 MongoClient.connect(url, { useUnifiedTopology: true }, function (err, client) {
