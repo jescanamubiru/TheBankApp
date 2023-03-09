a90b2375c396f7246287419988c2b5637d5db77d
namubirujesca <namubirujesca@gmail.com>
2023-03-08 21:26:59 -0600

    debug db connection

--- repository.js
+++ repository.js
@@ -1,79 +1,79 @@
-const MongoClient = require('mongodb').MongoClient;
+const MongoClient = require("mongodb").MongoClient;
 // const url = 'mongodb://localhost:27017';
 // const url = 'mongodb://mongo:27017/dockerize-me';
-const url = 'mongodb://mongo:27017';
+const url = "mongodb://mongo:27017";
 let db = null;
 // connect to mongo
-MongoClient.connect(url, { useUnifiedTopology: true }, function (err, client) {
-    console.log("Connected successfully to db server");
+MongoClient.connect(url, { useUnifiedTopology: true }, (err, client) => {
+  if (err) {
+    console.log('err>>', err);
+  }
+  console.log("Connected successfully to db server");
 
-    // connect to jbankingdb database
-    db = client.db('jbankingdb');
+  // connect to jbankingdb database
+  db = client.db("jbankingdb");
 });
 
 // create user account using the collection.insertOne function
 const create = (name, email, password) => {
-    return new Promise((resolve,reject)=> {
-        const collection = db.collection('users');
-        const doc = {name,email,password,balance:0};
-        collection.insertOne(doc,{w:1},function(err, result){
-            err ? reject(err) : resolve(doc);
-        });
-    })
-}
+  return new Promise((resolve, reject) => {
+    const collection = db.collection("users");
+    const doc = { name, email, password, balance: 0 };
+    collection.insertOne(doc, { w: 1 }, function (err, result) {
+      err ? reject(err) : resolve(doc);
+    });
+  });
+};
 
-// find user account 
+// find user account
 const find = (email) => {
-    return new Promise((resolve, reject) => {
-        const customers = db
-            .collection('users')
-            .find({ email: email })
-            .toArray(function (err, docs) {
-                err ? reject(err) : resolve(docs);
-            });
-    })
-}
+  return new Promise((resolve, reject) => {
+    const customers = db
+      .collection("users")
+      .find({ email: email })
+      .toArray(function (err, docs) {
+        err ? reject(err) : resolve(docs);
+      });
+  });
+};
 
 // find user account
 const findOne = (email) => {
-    return new Promise((resolve, reject) => {
-        const customers = db
-            .collection('users')
-            .findOne({ email: email })
-            .then((doc) => resolve(doc))
-            .catch((err) => reject(err));
-    })
-}
+  return new Promise((resolve, reject) => {
+    const customers = db
+      .collection("users")
+      .findOne({ email: email })
+      .then((doc) => resolve(doc))
+      .catch((err) => reject(err));
+  });
+};
 
 // update - deposit/withdraw amount
 const update = (email, amount) => {
-    return new Promise((resolve, reject) => {
-        const customers = db
-            .collection('users')
-            .findOneAndUpdate(
-                { email: email },
-                { $inc: { balance: amount } },
-                { returnOriginal: false },
-                function (err, documents) {
-                    err ? reject(err) : resolve(documents);
-                }
-            );
-
-
-    });
-}
+  return new Promise((resolve, reject) => {
+    const customers = db
+      .collection("users")
+      .findOneAndUpdate(
+        { email: email },
+        { $inc: { balance: amount } },
+        { returnOriginal: false },
+        function (err, documents) {
+          err ? reject(err) : resolve(documents);
+        }
+      );
+  });
+};
 
 // return all users by using the collection.find method
 const all = () => {
-    return new Promise((resolve, reject) => {
-        const customers = db
-            .collection('users')
-            .find({})
-            .toArray(function(err,docs){
-                err ? reject(err): resolve(docs);
-            });
-        })
-}
-
+  return new Promise((resolve, reject) => {
+    const customers = db
+      .collection("users")
+      .find({})
+      .toArray(function (err, docs) {
+        err ? reject(err) : resolve(docs);
+      });
+  });
+};
 
 module.exports = { create, findOne, find, update, all };
